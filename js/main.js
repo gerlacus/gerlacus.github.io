a54427dbd314a6608040ef54c8cbf5ad7d849573
gerlacus <gerlacus@gmail.com>
2017-07-21 14:59:35 -0600

    revamped colors + fonts, began 'what I do' section

--- js/main.js
+++ js/main.js
@@ -15,6 +15,38 @@ function main() {
         $('.navbar').css('background-color', ('rgba(0, 0, 0, ' + Math.min(($(window).scrollTop() / 1000), 0.7) + ')'));
     });
 
+    $(window).on("resize", function() {
+        //$('.me-portrait').
+    });
+    
+    $('.slideshow1 img:gt(0)').hide();
+    $('.slideshow2 img:gt(0)').hide();
+
+    setInterval(function() {
+        $('.slideshow1 :first-child').fadeOut()
+            .next('img').fadeIn()
+            .end().appendTo('.slideshow1');
+    }, 3000);
+    
+    setInterval(function() {
+        $('.slideshow2 :first-child').fadeOut()
+            .next('img').fadeIn()
+            .end().appendTo('.slideshow2');
+    }, 3000);
+    
+/*
+    setInterval(function() {
+        $('.slideshow img:first')
+            .fadeout(100)
+            .next()
+            .fadeIn(1000)
+            .end()
+            .appendTo('.slideshow');
+    }, 3000);
+*/           
+
+
+
 }
 
 
