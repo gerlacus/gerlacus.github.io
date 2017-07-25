abcab46c4b2b25d97ac1a9de396f8c335de39323
gerlacus <gerlacus@gmail.com>
2017-07-25 16:41:36 -0600

    Added footer, cleaned up + improved header/navbar

--- js/main.js
+++ js/main.js
@@ -2,18 +2,8 @@ function main() {
     $('.page-banner').hide();
     $('.page-banner').fadeIn(1000);
 
-    //$('.navbar').background-color = rgba(0, 0, 0, ($(window).scrollTop / 100));
-    //$('.navbar').li.style.margin = ($(window).scrollTop / 100));
-    //$('.navbar').css('background-color', ('rgba(0, 0, 0, ' + $(window).scrollTop() + ')');
-
-
-    //while ($(window).scrollTop() < 1000) {
-    //}
-    //$('.navbar').css('margin', (($(window).scrollTop() / 10) + 'px'));
-    //$('.navbar').css('margin', $(window).scrollTop() + 'px');
-
     $(window).on("scroll", function() {
-        $('.navbar').css('background-color', ('rgba(0, 0, 0, ' + Math.min(($(window).scrollTop() / 1000), 0.7) + ')'));
+        $('.navbar').css('background-color', ('rgba(0, 0, 0, ' + Math.min((($(window).scrollTop() - 75) / 500), 0.7) + ')'));
     });
 
     $(window).on("resize", function() {
@@ -25,21 +15,24 @@ function main() {
         // Set offset (top) to same for both sides of window
     });
     
+    // Slideshows for main page
+    // Hide images
     $('.slideshow1 img:gt(0)').hide();
     $('.slideshow2 img:gt(0)').hide();
 
+    // Slideshow 1
     setInterval(function() {
         $('.slideshow1 :first-child').fadeOut()
             .next('img').fadeIn()
             .end().appendTo('.slideshow1');
     }, 3000);
     
+    // Slideshow 2
     setInterval(function() {
         $('.slideshow2 :first-child').fadeOut()
             .next('img').fadeIn()
             .end().appendTo('.slideshow2');
     }, 3000);
-
 }
 
 
