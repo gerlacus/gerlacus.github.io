fadcfe57667f592c7719a72174a944212a580cdd
gerlacus <gerlacus@gmail.com>
2017-08-01 16:44:20 -0600

    Mobile test 7i (scroll fade)

--- js/main.js
+++ js/main.js
@@ -14,8 +14,23 @@ function main() {
         $('.text-section').fadeIn(1000);
     }, 500);
 
-    $(window).on("scroll", function() {
+var lastScTop = 0;
+    $(window).on("scroll", function(event) {
+        // Set menu transparency based on how close to top (closer to top = more faded)
         $('.navbar').css('background-color', ('rgba(0, 0, 0, ' + Math.min((($(window).scrollTop() - 75) / 500), 0.7) + ')'));
+        // Set menu transparency according to whether scrolled up or down (up = "awake" and opaque, down = faded)
+        // Scroll up -> opaque
+        var scTop = $(this).scrollTop();
+        console.log('scTop: ', scTop);
+        console.log('lastScTop: ', lastScTop);
+        if (scTop > lastScTop) {
+            // Scroll down
+            $('.navbar li a').css('color', 'rgba(255, 255, 255, 0.4)');
+        } else {
+            // Scroll up
+            $('.navbar li a').css('color', 'rgba(255, 255, 255, 0.8)');
+        }
+        lastScTop = scTop;
     });
 
     $(window).on("resize", resizeElements);
@@ -37,18 +52,20 @@ function main() {
             .next('img').fadeIn()
             .end().appendTo('.slideshow2');
     }, 3000);
+
+
+
 }
 
 
 function resizeElements(repeated) {
     var remSize = $('html').css('font-size').replace('px', '');
-    console.log("resized");
     // Portrait
     $('.me-portrait-container').css('top', 40 - ($('.me-portrait').width()/(remSize*2)) + 'rem');
     // Text section
     $('.text-section .text-below-portrait').css('padding-top', (-4 + $('.me-portrait-container img').width()/(remSize*2)) + 'rem');
-    $('.embed-vid-1').css('height', (0 + $('.h-layout-inner-2a p').height()/14) + 'rem');
-    $('.embed-vid-2').css('height', (-4 + $('.h-layout-inner-2b p').height()/14) + 'rem');
+    $('.embed-vid-1').css('height', (-8 + ($('.h-layout-inner-2a p').height()/14)) + 'rem');
+    $('.embed-vid-2').css('height', (-8 + ($('.h-layout-inner-2b p').height()/14)) + 'rem');
     //$('.embed-vid').css('height', (0 + $('.embed-vid').prev().height()/14) + 'rem');
 
     // Re-hide slideshow images
